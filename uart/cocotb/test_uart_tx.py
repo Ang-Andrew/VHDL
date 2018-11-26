0acd9d4f09c795ccd613d8baef565c1423cb6346
Andrew Ang <andrewang321@gmail.com>
2018-11-26 22:18:21 +1100

    updated UART TX cocotb test with writing hello world

--- uart/cocotb/test_uart_tx.py
+++ uart/cocotb/test_uart_tx.py
@@ -1,7 +1,7 @@
-# Simple tests for an adder module
+# UART TX test
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import Timer, RisingEdge
+from cocotb.triggers import Timer, RisingEdge, FallingEdge
 from cocotb.result import TestFailure
 import random
 
@@ -19,12 +19,45 @@ def uart_tx_test(dut):
     
     # Send data to be sent to UART TX module with valid data input
     
-    dut.data_in <= 0xaf
-    dut.data_valid <= 1
+    dut.i_data_in <= 0x68
+    dut.i_data_valid <= 1
     
+    yield FallingEdge(dut.o_busy)
+    dut.i_data_in <= 0x65
+    
+    yield FallingEdge(dut.o_busy)
+    dut.i_data_in <= 0x6c
+    
+    yield FallingEdge(dut.o_busy)
+    dut.i_data_in <= 0x6c
+    
+    yield FallingEdge(dut.o_busy)
+    dut.i_data_in <= 0x6f
+    
+    yield FallingEdge(dut.o_busy)
+    dut.i_data_in <= 0x20
+    
+    yield FallingEdge(dut.o_busy)
+    dut.i_data_in <= 0x77
+    
+    yield FallingEdge(dut.o_busy)
+    dut.i_data_in <= 0x6f
+    
+    yield FallingEdge(dut.o_busy)
+    dut.i_data_in <= 0x72
+    
+    yield FallingEdge(dut.o_busy)
+    dut.i_data_in <= 0x6c
+    
+    yield FallingEdge(dut.o_busy)
+    dut.i_data_in <= 0x64
+    
+    yield FallingEdge(dut.o_busy)
+    dut.i_data_in <= 0x0a
+   
     # Print out serial stream out
     
-    yield Timer(100*868*8)
+    yield Timer(2)
     
     print('Simulation ended')
     
